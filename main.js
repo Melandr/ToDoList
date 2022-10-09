71048794127f3fd6ee6bb6e164ca7283ef6bb923
Melandr <melnychenko.a@gmail.com>
2022-10-09 14:19:45 +0300

    add drag & drop

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ const addTaskBtn = document.querySelector("#add-task-btn");
 const deskTaskInput = document.querySelector("#description-task");
 const tasksWrapper = document.querySelector(".tasks-wrapper");
 
-let tasks;
-//загружаем список задач из localStorage
-!localStorage.tasks ? (tasks = []) : (tasks = JSON.parse(localStorage.getItem("tasks")));
+//инициализируем массив tasks, или загружаем из localStorage
+let tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
 
 let taskItemElements = [];
 
@@ -17,7 +16,7 @@ function Task(description) {
 //функция, реализующая шаблон одной задачи
 const createTemplate = (task, index) => {
   return `
-    <div class="task-item ${task.completed ? "checked" : ""}">
+    <div class="task-item ${task.completed ? "checked" : ""}" draggable="true">
       <div class="description">${task.description}</div>
       <div class="buttons">
         <input onClick="completeTask(${index})" type="checkbox" class="btn-complete" ${
@@ -36,7 +35,7 @@ const filterTasks = () => {
   tasks = [...activeTasks, ...completedTasks];
 };
 
-//функция заполнения задач на странице
+//функция обновления списка задач на странице
 const updateHtmlList = () => {
   tasksWrapper.innerHTML = "";
   if (tasks.length > 0) {
@@ -77,9 +76,68 @@ const deleteTask = (index) => {
 };
 
 //обработчик клика по кнопке добавления задачи
-addTaskBtn.addEventListener("click", () => {
+addTaskBtn.addEventListener("click", addNewTask);
+
+//обработчик нажатия на клавишу Enter для добавления задачи
+deskTaskInput.addEventListener("keydown", (event) => {
+  if (deskTaskInput.value.length > 0 && event.key === "Enter") {
+    addNewTask();
+  }
+});
+
+//обработчик начала перетаскивания
+tasksWrapper.addEventListener("dragstart", (event) => {
+  event.target.classList.add("selected");
+});
+
+//обработчик конца перетаскивания
+tasksWrapper.addEventListener("dragend", (event) => {
+  event.target.classList.remove("selected");
+});
+
+const getNextElement = (cursorPosition, currentElement) => {
+  // Получаем объект с размерами и координатами
+  const currentElementCoord = currentElement.getBoundingClientRect();
+  // Находим вертикальную координату центра текущего элемента
+  const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+
+  // Если курсор выше центра элемента, возвращаем текущий элемент
+  // В ином случае — следующий DOM-элемент
+  const nextElement = cursorPosition < currentElementCenter ? currentElement : currentElement.nextElementSibling;
+
+  return nextElement;
+};
+
+//обработчик логики перетаскивания
+tasksWrapper.addEventListener("dragover", (event) => {
+  event.preventDefault();
+
+  //находим перемещаемый элемент
+  const activeElement = tasksWrapper.querySelector(".selected");
+  //находим элемент, над которым в данный момент находится курсор
+  const currentElement = event.target;
+  const isMoveable = activeElement !== currentElement && currentElement.classList.contains("task-item");
+
+  if (!isMoveable) {
+    return;
+  }
+
+  // evt.clientY — вертикальная координата курсора в момент,
+  // когда сработало событие
+  const nextElement = getNextElement(event.clientY, currentElement);
+
+  // Проверяем, нужно ли менять элементы местами
+  if ((nextElement && activeElement === nextElement.previousElementSibling) || activeElement === nextElement) {
+    // Если нет, выходим из функции, чтобы избежать лишних изменений в DOM
+    return;
+  }
+
+  tasksWrapper.insertBefore(activeElement, nextElement);
+});
+
+function addNewTask() {
   tasks.push(new Task(deskTaskInput.value));
   updateLocalStorage();
   updateHtmlList();
   deskTaskInput.value = "";
-});
+}
